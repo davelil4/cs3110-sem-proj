e2c670d851bd551c75d3e6b00ca91923fc0efaf7
David Lilienfeld <davidlilienfeld4@gmail.com>
2023-12-08 16:51:18 -0500

    Add add_order and remove_order

--- lib/order_book.ml
+++ lib/order_book.ml
@@ -1,9 +1,8 @@
-type asset = string
 type o_type = B | S
 
 type order = {
   o_type : o_type;
-  asset : asset;
+  asset : string;
   price : int;
   quantity : int;
   user : string;
@@ -28,71 +27,96 @@ type users = user StringMap.t
 let rec sort_book b = List.sort compare_order_asc b
 and compare_order_asc o1 o2 = compare o1.price o2.price
 
-let empty = StringMap.empty
+let empty = (StringMap.empty, StringMap.empty)
 
-let rec add_order book users order =
+let rec remove_order book users order =
+  let new_book = remove_from_book book order
+    in let new_users = remove_from_user_history (remove_from_user_pending users order) order
+       in (new_book, new_users)
+
+and remove_from_book book order =
+  match StringMap.find_opt order.asset book with
+  | Some {buy; sell} ->  begin
+    match order.o_type with 
+    | B -> StringMap.add order.asset { buy = List.filter (fun v -> v <> order) buy; sell = sell } book
+    | S -> StringMap.add order.asset {buy = buy; sell = List.filter (fun v -> v <> order) sell} book
+  end
+  | None -> book
+
+and remove_from_user_pending users order =
+  match StringMap.find_opt order.user users with
+      | Some u -> begin
+        StringMap.add order.user {
+          u with 
+          pending_orders = (remove_from_book u.pending_orders order)
+        } users
+      end
+      | None -> users
+
+and remove_from_user_history users order =
+  match StringMap.find_opt order.user users with
+      | Some u -> begin
+        StringMap.add order.user {
+          u with 
+          order_history = (remove_from_book u.order_history order)
+        } users
+      end
+      | None -> users
+
+let rec add_order book users order = 
   let new_book = add_to_book book order in
-  let new_users =
+  let new_users = add_to_users users order in
+  let bb = (best_bid new_book order.asset) in
+  let ba = (best_ask new_book order.asset) in
+    if bb.price > ba.price
+      then 
+        let b1, u1 = (remove_from_book new_book order, remove_from_user_pending new_users bb) in
+        let b2, u2 = (remove_from_book b1 order, remove_from_user_pending u1 bb) in
+        (b2, add_profit (add_profit u2 bb.user ~-(bb.price)) ba.user ba.price)
+    else
+      (new_book, new_users)
+
+  
+  
+and add_to_book book order = 
+  match StringMap.find_opt order.asset book with
+    | Some {buy; sell} ->  begin
+      match order.o_type with 
+      | B -> StringMap.add order.asset { buy = (sort_book (order :: buy)); sell = sell } book
+      | S -> StringMap.add order.asset { buy = buy; sell = (List.rev (sort_book (order :: sell))) } book
+    end
+    | None -> begin
+      match order.o_type with
+      | B -> StringMap.add order.asset { buy = [order]; sell = []} book
+      | S -> StringMap.add order.asset { buy=[]; sell=[order]} book
+    end
+
+  and add_to_users users order =
     StringMap.add order.user
       (match StringMap.find_opt order.user users with
-      | Some u ->
-          {
-            u with
-            pending_orders = add_to_book u.pending_orders order;
-            order_history = add_to_book u.order_history order;
-          }
-      | None ->
-          {
-            username = order.user;
-            order_history = add_to_book StringMap.empty order;
-            pending_orders = add_to_book StringMap.empty order;
-            profit = 0;
-          })
-      users
-  in
-  (new_book, new_users)
+      | Some u -> begin
+        {
+          u with 
+          pending_orders = (add_to_book u.pending_orders order); 
+          order_history = (add_to_book u.order_history order)
+        }
+      end
+      | None -> {
+        username=order.user;
+        order_history = (add_to_book StringMap.empty order);
+        pending_orders = (add_to_book StringMap.empty order);
+        profit=0;
+      }) users
 
-and add_to_book book order =
-  match StringMap.find_opt order.asset book with
-  | Some { buy; sell } -> (
-      match order.o_type with
-      | B ->
-          StringMap.add order.asset
-            { buy = sort_book (order :: buy); sell }
-            book
-      | S ->
-          StringMap.add order.asset
-            { buy; sell = List.rev (sort_book (order :: sell)) }
-            book)
-  | None -> (
-      match order.o_type with
-      | B -> StringMap.add order.asset { buy = [ order ]; sell = [] } book
-      | S -> StringMap.add order.asset { buy = []; sell = [ order ] } book)
 
-let rec remove_order book order =
-  match book with
-  | [] -> []
-  | h :: t -> if h = order then t else h :: remove_order t order
+and add_profit users un v =
+  let u = StringMap.find un users in let new_u =
+  { u with profit = (u.profit + v) } in 
+  (StringMap.add un new_u users)
 
-let best_bid asset_map asset_name =
-  match StringMap.find_opt asset_name asset_map with
-  | None -> None
-  (*Assume b is sorted *)
-  | Some (b, s) -> ( match b with h :: _ -> Some h | _ -> None)
-
-let best_ask asset_map asset_name =
-  match StringMap.find_opt asset_name asset_map with
-  | None -> None
-  (*Assume s is sorted *)
-  | Some (b, s) -> ( match s with h :: _ -> Some h | _ -> None)
-
-let find_user user book_and_users =
-  let u = snd book_and_users in
-  match StringMap.find_opt user u with None -> None | Some users -> Some users
 
 let to_list book = book
 let string_order_type ot = match ot with B -> "Buy" | S -> "Sell"
-(*let string_of_asset a = match a with Stock b -> b | Option b -> b*)
 
 let print_order o =
   Printf.printf
